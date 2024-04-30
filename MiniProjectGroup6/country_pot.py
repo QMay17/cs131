8996c43c1108156fd5c46babd32351d11f65d1f7
Jesse Ge <71725877+jesse1337@users.noreply.github.com>
2024-04-30 16:58:46 -0700

    country ratio

--- MiniProjectGroup6/country_pot.py
+++ MiniProjectGroup6/country_pot.py
@@ -9,25 +9,32 @@ df_second = df[(df['Non Pot'] >= 0.6)]
 
 plt.figure(figsize=(15, 8))  # Increase figure size
 plt.scatter(df_first['Country'], df_first['Non Pot'],
-            color='blue')  # Create scatter plot
-plt.xticks(rotation=90)  # Rotate the labels to fit them better
+            color='green')
+
+plt.xticks(rotation=90)
+
 plt.yticks([0, 0.3, 0.5, 0.7, 1])
 plt.xlabel('Country')
 plt.ylabel('Non-Potable Ratio')
 plt.title('Scatter Plot of Country vs. Non-potable Ratio by City Count')
-plt.tight_layout()  # Adjust layout to make room for label rotation
+
+plt.tight_layout()
+
 plt.savefig('pictures/NonPotabilityRatio_0-0.5.png')
 plt.close()
 
 
-plt.figure(figsize=(15, 8))  # Increase figure size
+plt.figure(figsize=(15, 8))
+
 plt.scatter(df_second['Country'], df_second['Non Pot'],
-            color='blue')  # Create scatter plot
-plt.xticks(rotation=90)  # Rotate the labels to fit them better
+            color='pink')
+plt.xticks(rotation=90)
 plt.yticks([0, 0.3, 0.5, 0.7, 1])
 plt.xlabel('Country')
 plt.ylabel('Non-Potable Ratio')
 plt.title('Scatter Plot of Country vs. Non-potable Ratio by City Count')
-plt.tight_layout()  # Adjust layout to make room for label rotation
+
+plt.tight_layout()
+
 plt.savefig('pictures/NonPotabilityRatio_0.5-1.png')
 plt.close()
